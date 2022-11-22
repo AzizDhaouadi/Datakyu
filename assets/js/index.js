7b507db8878c88f62146ca88a60dcd7ae4aed537
Aziz Dhaouadi <dhaziz.works@gmail.com>
2022-11-22 00:22:43 -0500

    Changed the js filed to close the offcanvas component when user clicks outside of it

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+const offCanvas = document.getElementById('offcanvasNavbar')
+offCanvas.addEventListener('hidePrevented.bs.offcanvas', (event) => {
+
+    const keepBodyPosition = () => {
+        event.preventDefault();
+        $('.offcanvas').offcanvas('hide');
+    }
+    keepBodyPosition();
+});
