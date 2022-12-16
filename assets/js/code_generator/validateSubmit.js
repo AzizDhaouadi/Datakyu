a32245b269f96d6ea104c770e5bd43ce9aa068de
Aziz Dhaouadi <dhaziz.works@gmail.com>
2022-12-16 18:13:14 -0500

    Added the logic for handling the event generation for SaaS events

--- assets/js/code_generator/validateSubmit.js
+++ assets/js/code_generator/validateSubmit.js
@@ -11,7 +11,29 @@ import * as ShareEvents from '../tracking_templates/general_events_modules/share
 
 import * as SignUpEvents from '../tracking_templates/general_events_modules/sign_up_module/signUpEvent.js'
 
+import * as saasAddPaymentInfoEvents from '../tracking_templates/saas_events_modules/add_payment_info_module/addPaymentInfoEvent.js'
 
+import * as saasAddUserEvents from '../tracking_templates/saas_events_modules/add_user_module/addUserEvent.js'
+
+import * as saasBeginCheckoutEvents from '../tracking_templates/saas_events_modules/begin_checkout_module/beginCheckoutEvent.js'
+
+import * as saasCreateAccountEvents from '../tracking_templates/saas_events_modules/create_account_module/createAccountEvent.js'
+
+import * as saasDeleteAccountEvents from '../tracking_templates/saas_events_modules/delete_account_module/deleteAccountEvent.js'
+
+import * as saasGenerateLeadEvents from '../tracking_templates/saas_events_modules/generate_lead_module/generateLeadEvent.js'
+
+import * as saasRemoveUserEvents from '../tracking_templates/saas_events_modules/remove_user_module/removeUserEvent.js'
+
+import * as saasSubscribeEvents from '../tracking_templates/saas_events_modules/subscribe_module/subscribeEvent.js'
+
+import * as saasTrialStartEvents from '../tracking_templates/saas_events_modules/trial_start_module/trialStartEvent.js'
+
+import * as saasTrialEndEvents from '../tracking_templates/saas_events_modules/trial_end_module/trialEndEvent.js'
+
+import * as saasTutorialBeginEvents from '../tracking_templates/saas_events_modules/tutorial_begin_module/tutorialBeginEvent.js'
+
+import * as saasTutorialCompleteEvents from '../tracking_templates/saas_events_modules/tutorial_complete_module/tutorialCompleteEvent.js'
 
 const codeGeneratorForm = document.getElementById('codeGeneratorForm');
 console.log("found the form");
@@ -116,7 +138,180 @@ codeGeneratorForm.addEventListener("submit", (event) => {
                     }
                 break;
 
-            }
+            }    
         break;
+
+        case "SaaS":
+            switch(chosenEventName) {
+                case "tutorial_begin":
+                    switch(chosenTrackingMedium) {
+                        case "dataLayer":
+                            codeConatainerSection.innerHTML = saasTutorialBeginEvents.dataLayerTutorialBeginEvent;
+                            break;
+                        case "gtag": 
+                            codeConatainerSection.innerHTML = saasTutorialBeginEvents.gtagTutorialBeginEvent;
+                            break;
+                        case "API":
+                            codeConatainerSection.innerHTML = saasTutorialBeginEvents.apiTutorialBeginEvent;
+                    }
+                break;
+
+                case "tutorial_complete":
+                    switch(chosenTrackingMedium) {
+                        case "dataLayer":
+                            codeConatainerSection.innerHTML = saasTutorialCompleteEvents.dataLayerTutorialCompleteEvent;
+                            break;
+                        case "gtag":
+                            codeConatainerSection.innerHTML = saasTutorialCompleteEvents.gtagTutorialCompleteEvent;
+                            break;
+                        case "API":
+                            codeConatainerSection.innerHTML = saasTutorialCompleteEvents.apiTutorialCompleteEvent;
+                    }
+                break;
+
+                case "add_payment_info":
+                    switch(chosenTrackingMedium) {
+                        case "dataLayer":
+                            codeConatainerSection.innerHTML = saasAddPaymentInfoEvents.dataLayerAddPaymentInfoEvent;
+                            break;
+                        case "gtag":
+                            codeConatainerSection.innerHTML = saasAddPaymentInfoEvents.gtagAddPaymentInfoEvent;
+                            break;
+                        case "API":
+                            codeConatainerSection.innerHTML = saasAddPaymentInfoEvents.apiAddPaymentInfoEvent;
+                            break;
+                        
+                    }
+                break;
+
+                case "begin_checkout":
+                    switch(chosenTrackingMedium) {
+                        case "dataLayer":
+                            codeConatainerSection.innerHTML = saasBeginCheckoutEvents.dataLayerBeginCheckoutEvent;
+                            break;
+                        case "gtag":
+                            codeConatainerSection.innerHTML = saasBeginCheckoutEvents.gtagBeginCheckoutEvent;
+                            break;
+                        case "API":
+                            codeConatainerSection.innerHTML = saasBeginCheckoutEvents.apiBeginCheckoutEvent;
+                            break;
+                    }
+                break;
+
+                case "generate_lead":
+                    switch(chosenTrackingMedium) {
+                        case "dataLayer":
+                            codeConatainerSection.innerHTML = saasGenerateLeadEvents.dataLayerGenerateLeadEvent;
+                            break;
+                        case "gtag":
+                            codeConatainerSection.innerHTML = saasGenerateLeadEvents.gtagGenerateLeadEvent;
+                            break;
+                        case "API":
+                            codeConatainerSection.innerHTML = saasGenerateLeadEvents.apiGenerateLeadEvent;
+                            break;
+                    }
+                break;
+
+                case "trial_start":
+                    switch(chosenTrackingMedium) {
+                        case "dataLayer":
+                            codeConatainerSection.innerHTML = saasTrialStartEvents.dataLayerTrialStartEvent;
+                            break;
+                        case "gtag":
+                            codeConatainerSection.innerHTML = saasTrialStartEvents.gtagTrialStartEvent;
+                            break;
+                        case "API":
+                            codeConatainerSection.innerHTML = saasTrialStartEvents.apiTrialStartEvent;
+                            break;
+                    }
+                break;
+
+                case "trial_end":
+                    switch(chosenTrackingMedium) {
+                        case "dataLayer":
+                            codeConatainerSection.innerHTML = saasTrialEndEvents.dataLayerTrialEndEvent;
+                            break;
+                        case "gtag":
+                            codeConatainerSection.innerHTML = saasTrialEndEvents.gtagTrialEndEvent;
+                            break;
+                        case "API":
+                            codeConatainerSection.innerHTML = saasTrialEndEvents.apiTrialEndEvent;
+                            break;
+                    }
+                break;
+
+                case "add_user":
+                    switch(chosenTrackingMedium) {
+                        case "dataLayer":
+                            codeConatainerSection.innerHTML = saasAddUserEvents.dataLayerAddUserEvent;
+                            break;
+                        case "gtag":
+                            codeConatainerSection.innerHTML = saasAddUserEvents.gtagAddUserEvent;
+                            break;
+                        case "API":
+                            codeConatainerSection.innerHTML = saasAddUserEvents.apiAddUserEvent;
+                            break;
+                    }
+                break;
+
+                case "remove_user":
+                    switch(chosenTrackingMedium) {
+                        case "dataLayer":
+                            codeConatainerSection.innerHTML = saasRemoveUserEvents.dataLayerRemoveUserEvent;
+                            break;
+                        case "gtag":
+                            codeConatainerSection.innerHTML = saasRemoveUserEvents.gtagRemoveUserEvent;
+                            break;
+                        case "API":
+                            codeConatainerSection.innerHTML = saasRemoveUserEvents.apiRemoveUserEvent;
+                            break;
+                    }
+                break;
+
+                case "create_account":
+                    switch(chosenTrackingMedium) {
+                        case "dataLayer":
+                            codeConatainerSection.innerHTML = saasCreateAccountEvents.dataLayerCreateAccountEvent;
+                            break;
+                        case "gtag":
+                            codeConatainerSection.innerHTML = saasCreateAccountEvents.gtagCreateAccountEvent;
+                            break;
+                        case "API":
+                            codeConatainerSection.innerHTML = saasCreateAccountEvents.apiCreateAccountEvent;
+                            break;
+                    }
+                break;
+
+                case "delete_account":
+                    switch(chosenTrackingMedium) {
+                        case "dataLayer":
+                            codeConatainerSection.innerHTML = saasDeleteAccountEvents.dataLayerDeleteAccountEvent;
+                            break;
+                        case "gtag":
+                            codeConatainerSection.innerHTML = saasDeleteAccountEvents.gtagDeleteAccountEvent;
+                            break;
+                        case "API":
+                            codeConatainerSection.innerHTML = saasDeleteAccountEvents.apiDeleteAccountEvent;
+                            break;
+                    }
+                break;
+
+                case "subscribe":
+                    switch(chosenTrackingMedium) {
+                        case "dataLayer":
+                            codeConatainerSection.innerHTML = saasSubscribeEvents.dataLayerSubscribeEvent;
+                            break;
+                        case "gtag":
+                            codeConatainerSection.innerHTML = saasSubscribeEvents.gtagSubscribeEvent;
+                            break;
+                        case "API":
+                            codeConatainerSection.innerHTML = saasSubscribeEvents.apiSubscribeEvent;
+                            break;
+                    }
+                break;
+
+
+            }
+        break;     
     }
 });
