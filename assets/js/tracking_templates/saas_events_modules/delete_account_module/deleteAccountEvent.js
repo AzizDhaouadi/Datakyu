0fa1ce677d8d9d08e041bc6b1e76c940cf22eae6
Aziz Dhaouadi <dhaziz.works@gmail.com>
2022-12-16 15:04:56 -0500

    Added the delete_user module

--- assets/js/tracking_templates/saas_events_modules/delete_account_module/deleteAccountEvent.js
+++ assets/js/tracking_templates/saas_events_modules/delete_account_module/deleteAccountEvent.js
@@ -1,33 +1,32 @@
-let dataLayerCreateAccountEvent = `
+let dataLayerDeleteAccountEvent = `
     window.dataLayer = window.dataLayer || [];
     <br>
     window.dataLayer.push({
         <br>
-        <span class="indent">'event': 'add_user'</span>,
+        <span class="indent">'event': 'delete_user'</span>,
         <br>
         <span class="indent">'organization_id': 'orgNum1'</span>
         <br>
     });`;
 
-let gtagCreateAccountEvent = `
-    gtag('event', 'add_user', {
+let gtagDeleteAccountEvent = `
+    gtag('event', 'delete_user', {
         <br>
         <span class="indent">'organization_id': 'orgNum1'</span>
         <br>
     });`;
 
-let apiCreateAccountEvent = `
+let apiDeleteAccountEvent = `
 'const measurement_id' = 'G-XXXXXXXXXX';
 <br>
 'const api_secret' = 'secret_value';
 <br>
-
 fetch(\`https://www.google-analytics.com/mp/collect?measurement_id=\${measurement_id}&api_secret=\${api_secret}\`, {<br>
   method: "POST", <br>
   body: JSON.stringify({ <br>
     client_id: 'XXXXXXXXXX.YYYYYYYYYY', <br>
     events: [{ <br>
-      name: 'add_user', <br>
+      name: 'delete_user', <br>
       params: { <br>
         'organization_id': 'orgNum1' <br>
       } <br>
@@ -35,4 +34,4 @@ fetch(\`https://www.google-analytics.com/mp/collect?measurement_id=\${measuremen
   }) <br>
 });`;
 
-export {dataLayerCreateAccountEvent, gtagCreateAccountEvent, apiCreateAccountEvent};
+export {dataLayerDeleteAccountEvent, gtagDeleteAccountEvent, apiDeleteAccountEvent};
