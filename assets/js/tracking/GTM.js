05657cc26302204a08429a4f40c59c77f2872914
Aziz Dhaouadi <dhaziz.works@gmail.com>
2022-12-27 17:15:09 -0500

    Added tracking to the container templates page

---
+++ assets/js/tracking/GTM.js
@@ -0,0 +1,60 @@
+// Getting the header CTA buttons & Sending a dataLayer event
+const headerCTAButtons = document.querySelectorAll('a.my-2');
+headerCTAButtons.forEach((headerCTAButton) => {
+    headerCTAButton.addEventListener('click', (e) => {
+        window.dataLayer = window.dataLayer || [];
+        window.dataLayer.push({
+            'event': 'clicked_header_cta',
+            'cta_text': `${e.target.textContent}`
+        });
+    });
+});
+
+// Getting the view configuration buttons and sending a dataLayer event
+const viewConfigurationButtons = document.querySelectorAll('div.btn-group button');
+viewConfigurationButtons.forEach((viewConfigurationButton) => {
+    viewConfigurationButton.addEventListener('click', (e) => {
+        window.dataLayer = window.dataLayer || [];
+        window.dataLayer.push({
+            'event': 'clicked_view_configuration_button',
+            'container_type': `${e.target.getAttribute('data-bs-target')}`
+        });
+    });
+});
+
+// Getting the download buttons and sending a dataLayer event
+const DownloadButtons = document.querySelectorAll('div.btn-group a');
+DownloadButtons.forEach((viewDownloadButton) => {
+    viewDownloadButton.addEventListener('click', (e) => {
+        window.dataLayer = window.dataLayer || [];
+        window.dataLayer.push({
+            'event': 'clicked_download_button',
+            'file_name': `${e.target.getAttribute('href')}`
+        });
+    });
+});
+
+// Getting the accordion items and sending a dataLayer event
+const accordionItems = document.querySelectorAll('div.accordion-item');
+accordionItems.forEach((accordionItem) => {
+    accordionItem.addEventListener('click', () => {
+        window.dataLayer = window.dataLayer || [];
+        window.dataLayer.push({
+            'event': 'clicked_accordion_item',
+            'accordion_title': `${accordionItem.querySelector('h2').textContent.trim()}`,
+            'accordion_content': `${accordionItem.querySelector('div.accordion-body').textContent.trim()}`
+        });
+    });
+});
+
+// Getting the nav links and sending a dataLayer event
+const navLinks = document.querySelectorAll('a.nav-link');
+navLinks.forEach((navLink) => {
+    navLink.addEventListener('click', (e) => {
+        window.dataLayer = window.dataLayer || [];
+        window.dataLayer.push({
+             'event': 'clicked_nav_link',
+             'link': `${e.target.textContent}`
+        });
+    });
+});
