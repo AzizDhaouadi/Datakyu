04d02be64668ccac479d4d9173e6e8bb16428888
Aziz Dhaouadi <dhaziz.works@gmail.com>
2023-01-21 15:45:36 -0500

    Added a typed.js file to handle the header typing animation

---
+++ assets/js/general_files/migrationTyped.js
@@ -0,0 +1,13 @@
+const migraitonHeaderOriginalText = "team.";
+
+var migrationTextOptions = {
+    strings: [migraitonHeaderOriginalText,'business.', 'startup.'
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    showCursor: true,
+    smartBackspace: true,
+    loop: true
+  };
+  
+  var migrationTexttyped = new Typed('#typed', migrationTextOptions);
