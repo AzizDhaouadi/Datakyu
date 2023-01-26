0fc01912bac1ff1febe60e00854bd78586408277
Aziz Dhaouadi <dhaziz.works@gmail.com>
2023-01-26 17:57:57 -0500

    Added tracking to the consulting web page

---
+++ assets/js/tracking/a-la-carte.js
@@ -0,0 +1,37 @@
+// Fire dataLayer events for the clicks on the Typeform button and link
+const typeformElements = document.querySelectorAll('[data-tf-popup]');
+typeformElements.forEach((typeformElement) => {
+    typeformElement.addEventListener('click', (e) => {
+        console.log('element clicked');
+        window.dataLayer = window.dataLayer || [];
+        window.dataLayer.push({
+            'event': 'initiated_contact_form',
+            'event_session_number': current_session_number,
+            'cta_text': e.target.textContent
+        });
+    });
+});
+
+//Fire dataLayer events for the learn more buttons
+const learnMoreButtons = document.querySelectorAll('button[data-bs-toggle="modal"]');
+learnMoreButtons.forEach((learnMoreButton) => {
+    learnMoreButton.addEventListener('click', () => {
+        window.dataLayer = window.dataLayer || [];
+        window.dataLayer.push({
+            'event': 'clicked_learn_more',
+            'offered_service': learnMoreButton.parentElement.querySelector('.card-title').textContent
+        });
+    });
+});
+
+//Fire event when user clicks on the different process steps
+const processSteps = document.querySelectorAll('a.step-content-wrapper');
+processSteps.forEach((processStep) => {
+    processStep.addEventListener('click', (e) => {
+        window.dataLayer = window.dataLayer || [];
+        window.dataLayer.push({
+            'event': 'clicked_process_step',
+            'step_title': processStep.querySelector('.step-title').innerText
+        });
+    });
+});
