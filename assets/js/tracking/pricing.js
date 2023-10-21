f9397c18a5bfa08cbd39c422becc5ef98615205e
Aziz Dhaouadi <dhaziz.works@gmail.com>
2023-10-20 21:52:28 -0400

    Added tracking for the contact buttons

--- assets/js/tracking/pricing.js
+++ assets/js/tracking/pricing.js
@@ -2,7 +2,7 @@ import { controlContactModule } from "../modules/contact.module.js"
 
 //Measurement ID
 const measurementID = "G-00VKM23QFH"
-let current_session_number
+let current_session_number = ""
 
 //Getting the session_number parameter from the gtag library
 let sessionNumberPromise = new Promise((resolve) => {
@@ -17,7 +17,7 @@ sessionNumberPromise.then((session_number) => {
 const packageButtons = document.querySelectorAll('[data-gtm-att]:not([data-gtm-att="contact"])')
 
 if (packageButtons == "undefined") {
-  throw new Error("Package buttons not found");
+  throw new Error("Package buttons not found")
 }
 
 const logPackageButtonClick = (button) => {
@@ -31,7 +31,22 @@ const logPackageButtonClick = (button) => {
 
 packageButtons.forEach((packageButton) => {
   packageButton.addEventListener("click", (e) => {
-    logPackageButtonClick(e);
-    controlContactModule();
+    logPackageButtonClick(e)
+    controlContactModule()
+  })
+})
+
+const contactButtons = document.querySelectorAll('[data-gtm-att="contact"]')
+if (contactButtons == "undefined") {
+  throw new Error("Contact buttons not found")
+}
+
+contactButtons.forEach((contactButton) => {
+  contactButton.addEventListener('click', () => {
+    window.dataLayer = window.dataLayer || []
+    window.dataLayer.push({
+        event: "clicked_contact_us",
+        event_session_number: current_session_number,
+    });
   });
 })
