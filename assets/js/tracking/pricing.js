b53ae2da848c0cdafd3a4e30ca4cd16c769e8ad8
Aziz Dhaouadi <dhaziz.works@gmail.com>
2023-10-20 20:49:47 -0400

    Changed the tracking of package buttons

--- assets/js/tracking/pricing.js
+++ assets/js/tracking/pricing.js
@@ -1,38 +1,37 @@
+import { controlContactModule } from "../modules/contact.module.js"
+
 //Measurement ID
-const measurementID = 'G-00VKM23QFH';
-let current_session_number;
+const measurementID = "G-00VKM23QFH"
+let current_session_number
 
 //Getting the session_number parameter from the gtag library
-let sessionNumberPromise = new Promise(resolve => {
-    gtag('get', measurementID, 'session_number', resolve)
-  });
-  
-  sessionNumberPromise.then((session_number) => {
-    current_session_number = session_number;
-  });
+let sessionNumberPromise = new Promise((resolve) => {
+  gtag("get", measurementID, "session_number", resolve)
+})
+
+sessionNumberPromise.then((session_number) => {
+  current_session_number = session_number
+})
 
 //Fire event when the user clicks on the package buttons
-const packageButtons = document.querySelectorAll('button[id]');
+const packageButtons = document.querySelectorAll('[data-gtm-att]:not([data-gtm-att="contact"])')
+
+if (packageButtons == "undefined") {
+  throw new Error("Package buttons not found");
+}
+
+const logPackageButtonClick = (button) => {
+  window.dataLayer = window.dataLayer || []
+  window.dataLayer.push({
+    event: "clicked_package_button",
+    package_type: button?.id,
+    event_session_number: current_session_number,
+  })
+}
+
 packageButtons.forEach((packageButton) => {
-    packageButton.addEventListener('click', (e) => {
-        window.dataLayer = window.dataLayer || [];
-        window.dataLayer.push({
-            'event': 'clicked_package_button',
-            'package_type': e.target.getAttribute('id'),
-            'event_session_number': current_session_number
-        });
-    });
-});
-
-//Fire event when the user clicks on the package buttons
-const salesButtons = document.querySelectorAll('a[data-gtm-att]');
-salesButtons.forEach((salesButton) => {
-    salesButton.addEventListener('click', (e) => {
-        window.dataLayer = window.dataLayer || [];
-        window.dataLayer.push({
-            'event': 'clicked_package_button',
-            'package_type': e.target.getAttribute('data-gtm-att'),
-            'event_session_number': current_session_number
-        });
-    });
-});
+  packageButton.addEventListener("click", (e) => {
+    logPackageButtonClick(e);
+    controlContactModule();
+  });
+})
