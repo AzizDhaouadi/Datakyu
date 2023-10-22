5b0ede7db4fef12321f86de837eaab54c7fcff5f
Aziz Dhaouadi <dhaziz.works@gmail.com>
2023-10-21 23:25:15 -0400

    Formatted the file

--- assets/js/modules/contact.module.js
+++ assets/js/modules/contact.module.js
@@ -5,15 +5,16 @@ const controlContactModule = () => {
     const openContactModuleButtons = document.querySelectorAll('[data-lead-capture-modal-button]');
     const closeContactModuleButton = document.querySelector('[data-close-lead-capture-modal]');
 
-    if(contactModule === "undefined" || contactModule === null) {
+    if (contactModule === "undefined" || contactModule === null) {
         throw new Error('Contact module not found');
     }
 
     closeContactModuleButton.addEventListener('click', () => contactModule.close());
 
-    for(let openContactModuleButton of openContactModuleButtons) {
+    for (let openContactModuleButton of openContactModuleButtons) {
         openContactModuleButton.addEventListener('click', () => contactModule.showModal());
     }
+
 }
 
-export {controlContactModule};
+export { controlContactModule };
