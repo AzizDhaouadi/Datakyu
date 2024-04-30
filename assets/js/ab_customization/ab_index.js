912cf61c692d033e2f211437dd339a6435c22dc9
Aziz Dhaouadi <dhaziz.works@gmail.com>
2024-04-30 19:37:26 -0400

    Added Snitcher Callback function

--- assets/js/ab_customization/ab_index.js
+++ assets/js/ab_customization/ab_index.js
@@ -1,18 +1,33 @@
-const homePageCustomization = () => {
-    if (!identification || identification.type !== 'business') {
-        return;
+function enhanceUserProfile(identification) {
+    if (identification && identification.type !== "isp") {
+        var reveal = identification.company;
+
+        window.dataLayer = window.dataLayer || [];
+
+        dataLayer.push({
+            event: 'snitcher_data_ready',
+            snitcher: {
+                source: "snitcher",
+                website: identification.domain || undefined,
+                type: identification.type || undefined,
+                location: reveal.location || undefined,
+                founded_year: reveal.founded_year || undefined,
+                company_name: reveal.name || undefined,
+                company_type: identification.type || undefined,
+                country: identification.geoIP.country || undefined,
+                country_code: identification.geoIP.country_code || undefined,
+                state: identification.geoIP.state || undefined,
+                city: identification.geoIP.city || undefined,
+                industry: reveal.industry || undefined,
+                number_of_employees: reveal.employee_range || undefined,
+            }
+        });
     }
 
-    const companyDetails = identification?.company;
 
-    // Start customization
-    const headerOne = document.querySelector('h1');
-    headerOne.innerText = `Make ${companyDetails?.name}'s data work harder with Datakyu.`
-
-    $('h1').fadeIn("slow");
 }
 
 window.SpotterSettings = {
     token: "1447|QbGVdI2HTR0eo2XXXQuHeUYqfvxS5ORuAjn98u3zcf0bb21a",
-    callback: homePageCustomization,
+    callback: enhanceUserProfile,
 };
