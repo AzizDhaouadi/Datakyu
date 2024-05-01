fd4fb715ba24b0cdee0dd4b826321e37da26fd72
Aziz Dhaouadi <dhaziz.works@gmail.com>
2024-04-30 21:25:03 -0400

    Added console logs for debugging

--- assets/js/ab_customization/ab_index.js
+++ assets/js/ab_customization/ab_index.js
@@ -38,3 +38,13 @@ window.SpotterSettings = {
 };
 
 console.log("Script loaded");
+
+!function (s, n, i, t, c, h) {
+    s.SnitchObject = i; s[i] || (s[i] = function () {
+        (s[i].q = s[i].q || []).push(arguments)
+    }); s[i].l = +new Date; c = n.createElement(t);
+    h = n.getElementsByTagName(t)[0]; c.src = '//snid.snitcher.com/8426066.js';
+    h.parentNode.insertBefore(c, h)
+}(window, document, 'snid', 'script');
+
+snid('verify', '8426066');
